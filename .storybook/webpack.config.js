312cfc24f58a582c2f93d1178596a87e9ecacf89
Pascal Duez <public@pascalduez.me>
2016-09-28 08:33:16 +0200

    Fix coverage include, stories css

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = function (baseConfig, configType) {
+
+  baseConfig.module.loaders.push({
+    test: /\.css?$/,
+    loaders: ['style', 'css?modules&localIdentName=[name]-[local]_[hash:base64:5]'],
+  });
+
+  return baseConfig;
+};
