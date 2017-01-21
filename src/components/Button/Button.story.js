d908ca89daf1a8a23e55df38141001ad3fe27265
Pascal Duez <public@pascalduez.me>
2017-01-21 11:33:30 +0100

    Add Flow to stories

--- src/components/Button/Button.story.js
+++ src/components/Button/Button.story.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import { withKnobs, text, select } from '@kadira/storybook-addon-knobs';
--- src/components/Header/Header.story.js
+++ src/components/Header/Header.story.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { withKnobs, text } from '@kadira/storybook-addon-knobs';
