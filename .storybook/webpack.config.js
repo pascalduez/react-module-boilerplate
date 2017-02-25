83ef3ee4f611e0c760d3018ddb8f7ff5bc2d2996
Pascal Duez <public@pascalduez.me>
2017-02-25 10:42:55 +0100

    Fix styleguide styles

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,18 @@
+/* eslint-disable no-param-reassign, global-require */
+
 module.exports = (baseConfig) => {
   baseConfig.module.loaders.push({
     test: /\.css?$/,
     loaders: [
       'style',
-      'css?modules&localIdentName=[name]-[local]_[hash:base64:5]',
+      'css?modules&localIdentName=[name]-[local]_[hash:base64:5]&importLoaders=1',
+      'postcss',
     ],
   });
+
+  baseConfig.postcss = [
+    require('../src/theme')(),
+  ];
 
   return baseConfig;
 };
