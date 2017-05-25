eaa42e9c9c2929497c08b16a878384559e41826c
Pascal Duez <public@pascalduez.me>
2017-05-25 17:33:52 +0200

    Implement custom properties preserve feature

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,14 +1,10 @@
 /* eslint-disable global-require */
 
-const theme = {
-  'color-primary': 'green',
-  'color-secondary': 'blue',
-  'color-danger': 'red',
-  'color-gray': '#ccc',
-};
+const theme = require('./theme');
 
 // Export configured plugin for use with `babel-plugin-css-modules-transform`.
 module.exports = () =>
   require('postcss-custom-properties')({
     variables: theme,
+    preserve: true,
   });
