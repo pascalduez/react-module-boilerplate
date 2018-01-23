ddab2575f776b807c86e902f0bd48baf00ab780f
Pascal Duez <public@pascalduez.me>
2018-01-23 11:49:46 +0100

    Simplify CI unit tests config

--- test/report.js
+++ test/report.js
@@ -1,7 +1,10 @@
 /* eslint-disable */
 
-// process.env.JEST_SUITE_NAME = 'react-module-boilerplate unit tests';
-// process.env.JEST_JUNIT_OUTPUT = './reports/junit-report.xml';
+process.env.JEST_SUITE_NAME = 'react-module-boilerplate unit tests';
+process.env.JEST_JUNIT_OUTPUT = './reports/junit-report.xml';
+
+process.env.TEST_REPORT_TITLE = 'react-module-boilerplate unit tests';
+process.env.TEST_REPORT_PATH = './reports/output-report.xml';
 
 module.exports = function (result) {
   require('jest-junit')(result);
