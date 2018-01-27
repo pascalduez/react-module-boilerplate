b44d1ff67113fbe345778d80212daf9e3bb40ea7
Pascal Duez <public@pascalduez.me>
2018-01-27 11:15:42 +0100

    Upgrade dependencies

--- test/report.js
+++ test/report.js
@@ -3,8 +3,8 @@
 process.env.JEST_SUITE_NAME = 'react-module-boilerplate unit tests';
 process.env.JEST_JUNIT_OUTPUT = './reports/junit-report.xml';
 
-process.env.TEST_REPORT_TITLE = 'react-module-boilerplate unit tests';
-process.env.TEST_REPORT_PATH = './reports/output-report.xml';
+process.env.JEST_HTML_REPORTER_PAGE_TITLE = 'react-module-boilerplate unit tests';
+process.env.JEST_HTML_REPORTER_OUTPUT_PATH = './reports/output-report.xml';
 
 module.exports = function (result) {
   require('jest-junit')(result);
