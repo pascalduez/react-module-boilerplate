37c5841c8d92254144b24dfff8cca8b2735f95aa
Pascal Duez <public@pascalduez.me>
2018-02-03 11:20:02 +0100

    Make Storybook work with babel 7

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,6 +1,20 @@
 /* eslint-disable no-param-reassign, global-require */
 
 module.exports = baseConfig => {
+  // Replace storybook baseConfig rule.
+  baseConfig.module.rules.splice(0, 1, {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          presets: ['./babel.config.js'],
+        },
+      },
+    ],
+  });
+
   baseConfig.module.rules.push({
     test: /\.css$/,
     use: [
