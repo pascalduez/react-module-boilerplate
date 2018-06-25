5f351f67487f72653104750ca75127030ed4aa12
Pascal Duez <public@pascalduez.me>
2018-06-25 13:13:42 +0200

    Upgrade linting

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,7 @@
 /* @flow */
 /* eslint css-modules/no-unused-class: [2, { markAsUsed: ['primary', 'secondary', 'danger'] }] */
+/* eslint react/default-props-match-prop-types: ['error', { 'allowRequiredDefaults': true }] */
+/* eslint-disable react/button-has-type */
 
 import * as React from 'react';
 import classNames from 'classnames';
@@ -8,25 +10,27 @@ import styles from './Button.css';
 
 export type Props = {
   children: React.Node,
-  theme?: 'primary' | 'secondary' | 'danger',
+  theme: 'primary' | 'secondary' | 'danger',
+  type: 'submit' | 'button' | 'reset',
   handleClick?: (evt?: SyntheticEvent<HTMLButtonElement>) => void,
 };
 
-function Button({
-  children,
-  theme = 'primary',
-  handleClick,
-  ...extraProps
-}: Props) {
+function Button({ children, theme, type, handleClick, ...extraProps }: Props) {
   return (
     <button
       className={classNames(styles.root, styles[theme])}
       onClick={handleClick}
+      type={type}
       {...extraProps}
     >
       {children}
     </button>
   );
 }
+
+Button.defaultProps = {
+  theme: 'primary',
+  type: 'submit',
+};
 
 export default Button;
