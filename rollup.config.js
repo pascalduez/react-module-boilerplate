ad40062f942470d9387a730ff2cb714dfdfbf706
Pascal Duez <public@pascalduez.me>
2020-08-05 15:12:40 +0200

    Update Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,21 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
-import resolve from 'rollup-plugin-node-resolve';
+import externals from 'rollup-plugin-node-externals';
+import resolve from '@rollup/plugin-node-resolve';
 import flowEntry from 'rollup-plugin-flow-entry';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 
 let plugins = [
-  peerDepsExternal(),
+  externals(),
   postcss({
     modules: true,
-    extract: `dist/${pkg.name}.css`,
+    extract: `${pkg.name}.css`,
   }),
   babel({
     exclude: ['node_modules/**'],
-    runtimeHelpers: true,
+    babelHelpers: 'runtime',
   }),
   resolve(),
   commonjs(),
