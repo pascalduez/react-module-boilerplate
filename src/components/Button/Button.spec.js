6b6d10ebe5f08d9dcea5f04a1c1b1677fb67c16e
Pascal Duez <public@pascalduez.me>
2020-10-18 22:24:11 +0200

    Follow latest RTL trends

--- src/components/Button/Button.spec.js
+++ src/components/Button/Button.spec.js
@@ -1,24 +1,25 @@
 /* @flow */
 /* eslint css-modules/no-unused-class: [2, { markAsUsed: ['secondary', 'danger'] }] */
 
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
 import Button from './Button';
 import styles from './Button.css';
 
 describe('<Button>', () => {
   it('should render the default markup', () => {
-    const { getByText } = render(<Button>Test</Button>);
+    render(<Button>Test</Button>);
 
-    const button = getByText('Test');
+    const button = screen.getByText('Test');
 
     expect(button.tagName).toBe('BUTTON');
   });
 
   it('should render the default classes', () => {
-    const { getByText } = render(<Button>Test</Button>);
+    render(<Button>Test</Button>);
 
-    const button = getByText('Test');
+    const button = screen.getByText('Test');
 
     expect(button).toHaveClass(styles.root);
     expect(button).toHaveClass(styles.primary);
@@ -27,20 +28,20 @@ describe('<Button>', () => {
   // Which is equivalent to:
 
   it('should render the default markup and classes [snapshot]', () => {
-    const { container } = render(<Button>Test</Button>);
+    const { asFragment } = render(<Button>Test</Button>);
 
-    expect(container.firstChild).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 
   // Event handlers.
 
   it('should trigger click event handlers', () => {
     const spy = jest.fn();
-    const { getByText } = render(<Button onClick={spy}>Test</Button>);
+    render(<Button onClick={spy}>Test</Button>);
 
-    const button = getByText('Test');
+    const button = screen.getByText('Test');
 
-    fireEvent.click(button);
+    userEvent.click(button);
 
     expect(spy).toHaveBeenCalled();
   });
--- src/components/Header/Header.spec.js
+++ src/components/Header/Header.spec.js
@@ -1,24 +1,24 @@
 // @flow
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import Header from './Header';
 import styles from './Header.css';
 
 describe('<Header>', () => {
   it('should render the default markup', () => {
-    const { container, getByText } = render(<Header title="Test" />);
+    const { container } = render(<Header title="Test" />);
 
-    const title = getByText('Test');
+    const title = screen.getByText('Test');
 
     expect((container.firstChild: any).tagName).toBe('HEADER');
     expect(title.tagName).toBe('H1');
   });
 
   it('should render the default classes', () => {
-    const { container, getByText } = render(<Header title="Test" />);
+    const { container } = render(<Header title="Test" />);
 
-    const title = getByText('Test');
+    const title = screen.getByText('Test');
 
     expect(container.firstChild).toHaveClass(styles.root);
     expect(title).toHaveClass(styles.title);
@@ -27,8 +27,8 @@ describe('<Header>', () => {
   // Which is equivalent to:
 
   it('should render the default markup and classes [snapshot]', () => {
-    const { container } = render(<Header title="Test" />);
+    const { asFragment } = render(<Header title="Test" />);
 
-    expect(container.firstChild).toMatchSnapshot();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
